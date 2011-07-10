59e501a943302e93fdbde8a517ef838ed411ca1c
dextructhorx <dextructhorx@ccf3e8ab-2cbd-c915-4ebb-0485294bdfca>
2011-07-10 08:15:04 +0000

    Added second part of hw3 and tests in the main function

--- basis.c
+++ basis.c
@@ -5,6 +5,9 @@
 #define N 8
 #define PI 3.14159265
 
+double cof(int x);
+void dct(unsigned char a[8][8]);
+
 int main(int argc, char *argv[])
 {
 	int u, v, x, y, i;
@@ -36,5 +39,76 @@ int main(int argc, char *argv[])
 		printf("\n");
 	}
 	
+	/*  Test for DCT calculation  */
+	unsigned char input[8][8];
+	for(u=0;u<8;u++){
+	   for(v=0; v<8; v++){
+	      input[u][v] = 0;		//matrix with all zero values
+	   }
+	}
+	dct(input);
+	for(u=0;u<8;u++){
+	   for(v=0; v<8; v++){
+	      input[u][v] = 255;	//matrix with all values = 255
+	   }
+	}
+	dct(input);
+	int random;
+        for(u=0;u<8;u++){
+	   for(v=0; v<8; v++){
+	      random = rand()%256;
+	      input[u][v] = random;	//matrix with random values 0 to 255
+	      srand(random+7);
+	   }
+	}
+	dct(input);
+
 	return 0;
 }
+
+//Part 2
+void dct(unsigned char a[8][8]){
+   double b[8][8];
+   double sum, incos1, incos2, n;
+   sum = 0;
+   n = 8;
+   int x, y, u, v;
+
+   printf("\n\tDCT Calculation\n\nInput matrix:\n");
+   for(u=0; u<8;u++){
+      for(v=0; v<8;v++){
+         printf("%3d ", a[u][v]);
+      }
+      printf("\n");
+   }
+
+   for(u=0; u<8;u++){
+      for(v=0; v<8;v++){
+         for(x=0; x<8;x++){
+            for(y=0; y<8;y++){
+               incos1 = (PI*u*((2*x)+1))/(2*n);
+               incos2 = (PI*v*((2*y)+1))/(2*n);
+               sum += (a[x][y]-128)*cos(incos1)*cos(incos2);
+            }
+         }
+         b[u][v] = (2/n)*cof(u)*cof(v)*sum;
+      }
+   }
+   printf("\nResulting matrix:\n");
+   for(u=0; u<8;u++){
+      for(v=0; v<8;v++){
+         printf("%f ", b[u][v]);
+      }
+      printf("\n");
+   }
+   printf("\n");
+}
+
+/* Function to calculate C(u) and C(v)*/
+double cof(int x){
+   if(x==0)
+      return (1/sqrt(2));
+   else
+      return 1.0;
+}
+
