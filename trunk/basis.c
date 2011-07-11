e889b4b5f0d6560754991b71c38f9794a877540e
dankodeih@gmail.com <dankodeih@gmail.com@ccf3e8ab-2cbd-c915-4ebb-0485294bdfca>
2011-07-11 19:17:54 +0000

    I changed part 3

--- trunk/basis.c
+++ trunk/basis.c
@@ -7,6 +7,7 @@
 
 double cof(int x);
 void dct(unsigned char a[8][8]);
+void dctInverse(float a[8][8]);
 
 int main(int argc, char *argv[])
 {
@@ -41,28 +42,34 @@ int main(int argc, char *argv[])
 	
 	/*  Test for DCT calculation  */
 	unsigned char input[8][8];
+	float input2[8][8];
 	for(u=0;u<8;u++){
 	   for(v=0; v<8; v++){
 	      input[u][v] = 0;		//matrix with all zero values
+	      input2[u][v] = 0;
 	   }
 	}
 	dct(input);
+	dctInverse(input2);
 	for(u=0;u<8;u++){
 	   for(v=0; v<8; v++){
 	      input[u][v] = 255;	//matrix with all values = 255
+	      input2[u][v] = 255;
 	   }
 	}
 	dct(input);
+	dctInverse(input2);
 	int random;
         for(u=0;u<8;u++){
 	   for(v=0; v<8; v++){
 	      random = rand()%256;
 	      input[u][v] = random;	//matrix with random values 0 to 255
+	      input2[u][v] = random;
 	      srand(random+7);
 	   }
 	}
 	dct(input);
-
+	dctInverse(input2);
 	return 0;
 }
 
@@ -92,6 +99,7 @@ void dct(unsigned char a[8][8]){
             }
          }
          b[u][v] = (2/n)*cof(u)*cof(v)*sum;
+
       }
    }
    printf("\nResulting matrix:\n");
@@ -102,7 +110,42 @@ void dct(unsigned char a[8][8]){
       printf("\n");
    }
    printf("\n");
+
 }
+//Part 3
+
+void dctInverse(float a[8][8]){
+  double b[8][8];
+   double sum, incos1, incos2, n;
+   sum = 0;
+   n = 8;
+   int x, y, u, v;
+
+   for(u=0; u<N;u++){
+      for(v=0; v<N;v++){
+         for(x=0; x<N;x++){
+            for(y=0; y<N;y++){
+               incos1 = (PI*u*((2*x)+1))/(2*n);
+               incos2 = (PI*v*((2*y)+1))/(2*n);
+               sum += (cos(incos1)*cos(incos2));
+
+            }
+         }
+	 b[u][v] = ((2/n)*cof(u)*cof(v)*sum) / (a[x][y]-128);
+
+      }
+   }
+   printf("\nResulting matrix (Inverse):\n");
+   for(u=0; u<n;u++){
+      for(v=0; v<n;v++){
+         printf("%f ", b[u][v]);
+      }
+      printf("\n");
+   }
+   printf("\n");
+
+}
+
 
 /* Function to calculate C(u) and C(v)*/
 double cof(int x){
