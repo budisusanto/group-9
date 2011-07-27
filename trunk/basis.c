86be164ed7b176f75fc2c95dba1401a9b9c5a345
dankodeih@gmail.com <dankodeih@gmail.com@ccf3e8ab-2cbd-c915-4ebb-0485294bdfca>
2011-07-27 23:14:33 +0000

    added new file (TEST)

--- trunk/basis.c
+++ trunk/basis.c
@@ -8,11 +8,12 @@
 double cof(int x);
 void dct(unsigned char a[8][8]);
 void dctInverse(float a[8][8]);
+float basis[8][8][8][8];
 
 int main(int argc, char *argv[])
 {
 	int u, v, x, y, i;
-	float basis[8][8][8][8];
+	
 	float xVal, yVal;
 
 	for(u = 0; u < N; u++)
@@ -20,6 +21,7 @@ int main(int argc, char *argv[])
 			for(x = 0; x < N; x++)
 				for(y = 0; y < N; y++)
 				{
+					// calculate cosine values
 					xVal = PI * u * ( 2 * x + 1 );
 					xVal /= 2 * N;
 					xVal = cos( xVal );
@@ -31,52 +33,37 @@ int main(int argc, char *argv[])
 					basis[u][v][x][y] = xVal * yVal;
 				}
 	
-	for(x = 0; x < N; x++)
-	{
-		for(y = 0; y < N; y++)
-		{
-			printf("%f ",basis[0][1][x][y]);
-		}
-		printf("\n");
-	}
 	
 	/*  Test for DCT calculation  */
 	unsigned char input[8][8];
-	float input2[8][8];
 	for(u=0;u<8;u++){
 	   for(v=0; v<8; v++){
 	      input[u][v] = 0;		//matrix with all zero values
-	      input2[u][v] = 0;
 	   }
 	}
 	dct(input);
-	dctInverse(input2);
 	for(u=0;u<8;u++){
 	   for(v=0; v<8; v++){
 	      input[u][v] = 255;	//matrix with all values = 255
-	      input2[u][v] = 255;
 	   }
 	}
 	dct(input);
-	dctInverse(input2);
 	int random;
         for(u=0;u<8;u++){
 	   for(v=0; v<8; v++){
 	      random = rand()%256;
 	      input[u][v] = random;	//matrix with random values 0 to 255
-	      input2[u][v] = random;
 	      srand(random+7);
 	   }
 	}
 	dct(input);
-	dctInverse(input2);
 	return 0;
 }
 
 //Part 2
 void dct(unsigned char a[8][8]){
    double b[8][8];
-   double sum, incos1, incos2, n;
+   double sum, n;
    sum = 0;
    n = 8;
    int x, y, u, v;
@@ -93,13 +80,13 @@ void dct(unsigned char a[8][8]){
       for(v=0; v<8;v++){
          for(x=0; x<8;x++){
             for(y=0; y<8;y++){
-               incos1 = (PI*u*((2*x)+1))/(2*n);
-               incos2 = (PI*v*((2*y)+1))/(2*n);
-               sum += (a[x][y]-128)*cos(incos1)*cos(incos2);
+		// sum up pixel values * basis
+               sum += (a[x][y]-128) * basis[u][v][x][y];
             }
          }
+	// apply 'C' function to u and v values, multiply by sum and 2/n
          b[u][v] = (2/n)*cof(u)*cof(v)*sum;
-
+	sum = 0;
       }
    }
    printf("\nResulting matrix:\n");
@@ -111,28 +98,36 @@ void dct(unsigned char a[8][8]){
    }
    printf("\n");
 
+   float tmp[8][8];                    // Copy for function Inverse
+   int inx, ind;
+   for(inx = 0; inx < 8; inx++){
+	for(ind = 0; ind < 8; ind++){	
+		tmp[inx][ind] = b[inx][ind];
+	}
+   }
+ 
+   dctInverse(tmp);
 }
 //Part 3
-
+//calculate inverse DCT from result of forward DCT
 void dctInverse(float a[8][8]){
   double b[8][8];
-   double sum, incos1, incos2, n;
+   double sum, n;
    sum = 0;
    n = 8;
    int x, y, u, v;
 
-   for(u=0; u<N;u++){
-      for(v=0; v<N;v++){
-         for(x=0; x<N;x++){
-            for(y=0; y<N;y++){
-               incos1 = (PI*u*((2*x)+1))/(2*n);
-               incos2 = (PI*v*((2*y)+1))/(2*n);
-               sum += (cos(incos1)*cos(incos2));
+   for(x=0; x<N;x++){
+      for(y=0; y<N;y++){
+         for(u=0; u<N;u++){
+            for(v=0; v<N;v++){
+               
+               sum += cof(u) * cof(v) * (a[u][v]) * basis[u][v][x][y];
 
             }
          }
-	 b[u][v] = ((2/n)*cof(u)*cof(v)*sum) / (a[x][y]-128);
-
+	 b[x][y] = ((2/n) * sum) + 128;
+	sum = 0;
       }
    }
    printf("\nResulting matrix (Inverse):\n");
